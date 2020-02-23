f3581d5b35256dd5dc0bb06fe583b212a2a5ff37
eduardo velasquez <eduardo.velasquezv@udea.edu.co>
2020-02-23 16:49:08 -0500

    microservicio actualizado y funcionando

---
+++ src/test/java/com/taxappy/eurakaServer/TaxappyEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taxappy.eurakaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxappyEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
